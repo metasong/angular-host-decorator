22f98890530def3448eeb9aefa798213931d7f72
metasong <metaseed@gmail.com>
2022-01-25 12:59:20 -0600

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ChildComponent } from './host/child.component';
-import { GrandChildComponent } from './host/grand-child.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
   declarations: [
-    AppComponent,
-    HelloComponent,
-    ChildComponent,
-    GrandChildComponent,
+    AppComponent
   ],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
